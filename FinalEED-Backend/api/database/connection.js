8b3a1e61867c3d96bad58c3b640d1ab438c2a8d1
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2023-04-21 19:38:23 +0300

    changes

--- FinalEED-Backend/api/database/connection.js
+++ FinalEED-Backend/api/database/connection.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import { config } from "dotenv";
+config();
 export default async function connect() {
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-    });
-    console.log("db connected");
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+  });
+  console.log("db connected");
 }
